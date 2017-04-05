f3efdf8ea0021d41e5ea4feea27c0a51ac9cc800
enkimute <enkimute@gmail.com>
2017-04-05 12:33:24 +0200

    Add files via upload

---
+++ tar.js
@@ -0,0 +1,21 @@
+var Tar = (function() {
+  var tarfiles = [];
+  return function(filename,complete) {
+    if (tarfiles[filename]) return complete&&complete(tarfiles[filename]),tarfiles[filename];
+    if (!filename.match(/\.tar|\.bin/i)) return complete&&complete(undefined);
+    var req = new XMLHttpRequest();
+    req.responseType = 'arraybuffer';
+    req.open('GET', filename, true);
+    req.onload = function() {
+      var u8 = new Uint8Array(req.response), format=[[0,100],[124,136]], pos=0;
+      while (pos < u8.byteLength) {
+        var asc = String.fromCharCode.apply(null,u8.slice(pos,pos+136)), header=format.map(function(f){return asc.slice(f[0],f[1]).replace(/\0/g,''); });
+        if (header[0]=="") break;
+        tarfiles[header[0]] = URL.createObjectURL(new Blob([u8.slice(pos+512,pos+512+parseInt(header[1],8))], {type:'application/octet-stream'}))+'#'+header[0];
+        pos += 512 + Math.ceil(parseInt(header[1],8)/512)*512;
+      }
+      complete&&complete(tarfiles);
+    };
+    req.send(null);
+  }; 
+})();
